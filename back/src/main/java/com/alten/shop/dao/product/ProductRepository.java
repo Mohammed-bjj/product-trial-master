85e50fea0bcd9f523afa588a30b41f8fff691ee1
mohammed <mohammedbouajaja1@gmail.com>
2025-08-29 01:04:28 +0200

    refact: adapt repository product

--- back/src/main/java/com/alten/shop/dao/product/ProductRepository.java
+++ back/src/main/java/com/alten/shop/dao/product/ProductRepository.java
@@ -1,6 +1,29 @@
 package com.alten.shop.dao.product;
 
 import com.alten.shop.utils.entities.product.Product;
+import com.alten.shop.utils.entities.product.InventoryStatus;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ProductRepository extends JpaRepository<Product, Long> { }
+import java.util.Optional;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE " +
+            "(:category IS NULL OR :category = '' OR LOWER(p.category) = LOWER(:category)) AND " +
+            "(:inventoryStatus IS NULL OR p.inventoryStatus = :inventoryStatus)")
+    Page<Product> findProductsWithFilters(
+            @Param("category") String category,
+            @Param("inventoryStatus") InventoryStatus inventoryStatus,
+            Pageable pageable
+    );
+    @Query("SELECT MAX(p.id) FROM Product p")
+    Long findMaxId();
+
+    Optional<Product> findByCode(String code);
+}
