56ef4062824ae69e12f8098a942cb9c7e404b50b
mohammed <mohammedbouajaja1@gmail.com>
2025-08-29 00:56:19 +0200

    feat: add pagination when we call get all product

--- back/src/main/java/com/alten/shop/utils/configuration/PaginationConfig.java
+++ back/src/main/java/com/alten/shop/utils/configuration/PaginationConfig.java
@@ -3,6 +3,9 @@ package com.alten.shop.utils.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import static org.springframework.data.domain.Sort.Direction;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,19 +16,25 @@ public class PaginationConfig {
     
     @Value("${app.pagination.default-size-public}")
     private int defaultSizePublic;
-    
+
     @Value("${app.pagination.default-size-admin}")
     private int defaultSizeAdmin;
-    
-    public Pageable createPageableForPublic(Integer page, Integer size) {
-        int actualPage = page != null ? page : defaultPage;
-        int actualSize = size != null ? size : defaultSizePublic;
-        return PageRequest.of(actualPage, actualSize);
+
+    @Value("${app.pagination.default-sort}")
+    private  String defaultSort;
+
+    public Pageable createPageableForPublic(Integer page, Integer size, String sort) {
+        return getPageable(page, size, sort, defaultSizePublic);
     }
     
-    public Pageable createPageableForAdmin(Integer page, Integer size) {
+    public Pageable createPageableForAdmin(Integer page, Integer size, String sort) {
+        return getPageable(page, size, sort, defaultSizeAdmin);
+    }
+
+    private Pageable getPageable(Integer page, Integer size, String sort, int defaultSizeAdmin) {
+        Direction direction = defaultSort.equalsIgnoreCase(sort) ? Direction.ASC : Direction.DESC;
         int actualPage = page != null ? page : defaultPage;
         int actualSize = size != null ? size : defaultSizeAdmin;
-        return PageRequest.of(actualPage, actualSize);
+        return PageRequest.of(actualPage, actualSize, Sort.by(direction, "price"));
     }
 }
